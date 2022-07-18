2fe4f90ab8a3e362382aa42678935620a799187e
Leonardo Chagas Pizzo <leonardo_pizzo@usp.br>
2022-07-18 20:04:22 -0300

    milestone 2 done

--- module-2/client.c
+++ module-2/client.c
@@ -1,151 +1,33 @@
-// C program for the Client Side
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <signal.h>
-
-
-// inet_addr
 #include <arpa/inet.h>
 #include <unistd.h>
-
-// For threading, link with lpthread
 #include <pthread.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <semaphore.h>
-
-#include "aux.h"
 
 // Message type
 #define MAX_MSG 4096
-#define READLINE_BUFFER 4096
 
-typedef struct Message_t {
-    char user[51];
-    int req;
-    char content[MAX_MSG];
-}Message_t;
+int quit = 0;
 
-#define CONTINUE 100
-#define QUIT 200
+// thread de recebimento de mensagens do servidor
+void* clientThread(void *sockID){
+	int user_socket = *((int*) sockID);
 
-int myWrite(int connfd, char *msg){
-	char rec[3];
-	bzero(rec, sizeof(rec));
-
-	// Sends the message
-	if(send(connfd, msg, sizeof(msg), 0) <= 0){
-		printf("erro no envio\n");
-		return -1;
+	while(!quit) {
+		char data[MAX_MSG];
+		int read = recv(user_socket, data, MAX_MSG, 0);
+		data[read] = '\0';
+		printf("%s\n", data);
 	}
-
-	// Waits for the AK
-	if(recv(connfd, rec, sizeof(rec), 0) <= 0){
-		printf("erro no recebimento da resposta\n");
-		return -1;
-	}
-	if(strncmp(rec, "AK", 2) != 0){
-		printf("erro, resposta nao e AK\n");
-		return -1;
-	}
-
-    return 0;
-}
-
-
-// Function to send data to
-// server socket.
-/*
-void* clienthread_read(void *param)
-{
-	//Message_t msg = *((Message_t*) param[0]);
-	//int network_socket = *((int*) param[1]);
-	
-	// receive data from the server
-	char input[4096];
-	send(network_socket, &msg.req,
-		sizeof(msg.req), 0);
-
-	recv(network_socket, &input, sizeof(input), 0);
-	printf("user: %s\n", input);
-	
-	bzero(input, sizeof(input));
-
-	recv(network_socket, &input, sizeof(input), 0);
-	printf("content: %s\n", input);
 
 	pthread_exit(NULL);
-
-	return 0;
-}
-
-// Function to send data to
-// server socket.
-void* clienthread_write(void *param)
-{
-	//Message_t msg = *((Message_t*) param[0]);
-	//int network_socket = *((int*)param[1]);
-
-	// Send data to the socket
-	send(network_socket, &msg.req,
-		sizeof(msg.req), 0);
-
-	//int erro = myWrite(networkSocket,);
-	
-	pthread_exit(NULL);
-
-	return 0;
-}*/
-
-char *readline(FILE *stream) {
-    char *string = (char *)calloc(MAX_MSG + 1, sizeof(char));
-    int pos = 0;
-    
-    do{
-        if (pos % MAX_MSG == 0) {
-            string = (char *)realloc(string, (pos / MAX_MSG + 1) * MAX_MSG);
-        }
-        string[pos] = (char)fgetc(stream);
-    }while(string[pos++] != '\n' && !feof(stream));
-    string[strlen(string)-1] = '\n';
-
-    return string;
-}
-
-int quit_server(int conn, int network_socket){
-	if (conn){
-		conn = 0;
-		close(network_socket);
-		printf("Connection Closed.\n");
-	}
-	
-	return QUIT;
-}
-
-int conct(int *network_socket){
-	// Create a stream socket
-	*network_socket = socket(AF_INET,
-							SOCK_STREAM, 0);
-
-	// Initialise port number and address
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = INADDR_ANY;
-	server_address.sin_port = htons(8989);
-
-	// Initiate a socket connection
-	int connection_status = connect(*network_socket,
-									(struct sockaddr*)&server_address,
-									sizeof(server_address));
-
-	// Check for connection error
-	if (connection_status < 0) {
-		puts("Error\n");
-		return 0;
-	}
-
-	printf("Connection established\n");
-	return 1;
 }
 
 // Handle 'ctrl + c' signal
@@ -153,100 +35,97 @@ void interruptionHandler(int dummy) {
     printf("\n'Ctrl + C' não é um comando válido!!\n");
 }
 
-void* clienthread_send(void** params){
-	int network_socket = *((int*)params[0]);
-	char user[51];
-	char input[4096];
-	strcpy(user, ((char*)params[1]));
-	strcpy(input, ((char*)params[2]));
-	int erro = 0;
-	
-	// Envia a mensagem em 3 partes -> a flag de identificaçao 
-	//		da msg, o usuario que enviou a mensagem e o conteudo dela
-	erro = myWrite(network_socket, "2");
-	erro = myWrite(network_socket, user);
-	erro = myWrite(network_socket, input);
-
-	
-	pthread_exit(NULL);
-}
-
 // Driver Code
 int main() {	
 	// set handler function
 	//signal(SIGINT, interruptionHandler);
 	
+	int user_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in serverAddr;
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8989);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int conn = 0, user_set = 0, tries = 0;
+	char input[MAX_MSG];
+
+	// cria uma thread para receber respostas do servidor
 	pthread_t tid;
-    int cmd = CONTINUE, conn = 0, network_socket, user_set = 0, exit = 0, erro = 0;
-	char user[51] = "", input[4096];
 
-	void* params[5];
-
-    while(cmd != QUIT) {
+	while(1) {
+		tries = 0;
 		bzero(input, sizeof(input));
-        strcpy(input, readline(stdin));
+		scanf("%[^\n]s", input);
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
 
-		if (feof(stdin)){
-			cmd = quit_server(conn, network_socket);
-			continue;
-		}
-    
-
-        // input that starts with / is likely a command
-        if (input[0] == '/') {
-			if(strncmp(input, "/quit", 5) == 0){
-				// Closes the connection
-				cmd = quit_server(conn, network_socket);
-			} else if (strncmp(input, "/ping", 5) == 0){
-				// Envia ping para o servidor 
-				// TODO botar em uma funcao
-				if(conn){
-					int req = 3;
-					send(network_socket, &req, sizeof(req), 0);
-					send(network_socket, "/ping", strlen("/ping")+1, 0);
-					bzero(input, sizeof(input));
-					recv(network_socket, &input, sizeof(input), 0);
-					printf("%s\n", input);
-				} else {
-					printf("Voce deve estar conectado ao servidor para enviar mensagens. Use o comando /connect.\n");
-				}
-			} else if (strncmp(input, "/connect", 8) == 0){
-				// Conecta ao server                    
-				conn = conct(&network_socket);
-			} else if (strncmp(input, "/nickname", 9) == 0){
-				// Muda o nick
-				strncpy(user, (input+10), strlen(input) - 11);
-				user_set = 1;
-			} else {
-				//Comando nao reconhecido
-				printf("Comando nao reconhecido.\n");
-				int req = 3;
-				send(network_socket, &req, sizeof(req), 0);
-				bzero(input, sizeof(input));
-				recv(network_socket, &input, sizeof(input), 0);
-				printf("%s\n", input);
+		// conecta ao servidor
+		if(strncmp(input, "/connect", 8) == 0){
+			if(conn){
+				printf("Voce ja esta conectado ao servidor...\n");
+				continue;
 			}
-        } else if (input[0] != '\0'){
-			// texto eh uma mensagem
+			if(connect(user_socket, (struct sockaddr*) &serverAddr, 
+									sizeof(serverAddr)) == 0){
+				printf("Conection established...\n");
+				conn = 1;
+				pthread_create(&tid, NULL, clientThread, (void *) &user_socket);
+			}
+			else
+				printf("Conection error...\n");
+
+		} 
+		// envia ping ao servidor
+		else if (strncmp(input, "/ping", 5) == 0){
+			send(user_socket, "/ping", 5, 0);
+		} 
+
+		// fecha a conexao
+		else if (strncmp(input, "/quit", 5) == 0){
+			send(user_socket, "/quit", 5, 0);
+			quit = 1;
+			if(conn){
+				conn = 0;
+				close(user_socket);
+				printf("Connection closed...\n");
+			}
+			break;
+		}
+		// muda o nickname e envia para o servidor o novo nome
+		else if (strncmp(input, "/nickname", 9) == 0){
+			if(!conn) {
+				printf("Voce precisa estar conectado para definir um nome de usuario.\n");
+				continue;
+			}
+			printf("New User Name: %s\n", (input+10));
+			send(user_socket, (input+10), strlen(input), 0);
+			user_set = 1;
+		}
+		// envia a mensagem -> deve tentar 5 vezes no maximo antes de desistir
+		else if (input[0] != '\0') {
+			// checa se o cliente esta conectado ao servidor
 			if(!conn){
-				printf("Voce deve estar conectado ao servidor para enviar mensagens. Use o comando /connect.\n");
+				printf("Nao e possivel enviar uma mensagem sem estar conectado ao servidor. Use o comando /connect para se conectar.\n");
 				continue;
 			}
-			
+			// checa se o usuario ja possui um nome
 			if(!user_set){
-				printf("Voce deve definir um nome de usuario antes de poder enviar mensagens. Use o comando /nickname [nome] para mudar seu nome.\n");
+				printf("Nao e possivel enviar uma mensagem sem definir um nome de ususario. Use o comando /nickname [nome] para mudar seu nome.\n");
 				continue;
 			}
-			
-			params[0] = (void*)(&network_socket);
-			params[1] = (void*)(&user);
-			params[2] = (void*)(&input);
-
-			pthread_create(&tid, NULL,
-				clienthread_send,
-				&params);
+			// tenta enviar a mensagem ate 5 vezes se falhar desconecta o cliente
+			while(send(user_socket, input, MAX_MSG, 0) == -1 && tries++ <= 5);
+			if(tries == 5){
+				printf("Erro ao enviar a mensagem.\n");
+				if(conn)
+					close(user_socket);
+				break;
+			}
 		}
-    }
+	}
+	printf("cheguei aqui\n");
 	// Suspend execution of
 	// calling thread
 	pthread_join(tid, NULL);
--- module-2/server.c
+++ module-2/server.c
@@ -1,149 +1,93 @@
-// C program for the Server Side
-
-// inet_addr
 #include <arpa/inet.h>
-
-// For threading, link with lpthread
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <signal.h>
-#include <assert.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 
 #define MAX_MSG 4096
 
-int myRead(int connfd, char *msg){
-	bzero(msg, sizeof(msg));
-	// recebe a mensagem
-	if(recv(connfd, msg, sizeof(msg), 0) <= 0){
-		printf("erro no recebimento\n");
-		return -1;
+typedef struct user {
+	struct sockaddr_in socketAddr;
+	char name[51];
+	int sockID;
+	int len;
+	int index;
+	// TODO: adicionar uma variavel para guardar se o usuario ainda esta
+	//			conectado, caso contrario pular ele na hora de enviar mensagens
+} user;
+
+// TODO: modularizar o codigo
+// TODO: resolver o problema de Broken Pipe que acontece quando um dos clientes
+//		desconecta do servidor -> acho que e pq o servidor tenta enviar a mensagem
+// 		que aquele cliente saiu para ele mesmo, e como a conexao foi fechada
+//		um erro e gerado
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+// Array for thread
+struct user userList[1024];
+int user_count = 0;
+pthread_t thread[1024];
+
+// Thread de comunicaçao com o cliente
+void* clientThread(void *Client){
+	user *client = (user *)Client;
+
+	char data[MAX_MSG];
+	char msg[MAX_MSG];
+	// Recebe o nome de usuario desse cliente
+	int read = recv(client->sockID, data, MAX_MSG, 0);
+	data[read] = '\0';
+	strncpy(client->name, data, read);
+
+	// loop de comunicaçao do cliente
+	while(1) {
+		bzero(msg, MAX_MSG);
+
+		// Recebe a mensagem do usuario
+		bzero(data, MAX_MSG);
+		read = recv(client->sockID, data, MAX_MSG, 0);
+		data[read] = '\0';
+
+		// comandos especiais
+		if(strncmp(data, "/quit", 5) == 0) {
+			printf("%s saiu...\n", client->name);
+			for(int i=0; i<user_count; i++) {
+				strcpy(msg, client->name);
+				strcat(msg, " saiu");
+				// TODO: checar se o usuario ainda esta conectado antes de enviar a mensagem
+				send(userList[i].sockID, msg, MAX_MSG, 0);
+			}
+
+		} 
+		else if (strncmp(data, "/ping", 5) == 0) {
+			send(client->sockID, "pong", 4, 0);
+		} 
+		else if (strncmp(data, "/nickname", 9) == 0) {
+			strncpy(client->name, (data+10), (read-1));
+			printf("user: %s\n", client->name);
+		}
+
+		// mensagem normal
+		else {
+			for(int i=0; i<user_count; i++) {
+				strcpy(msg, client->name);
+				strcat(msg, ": ");
+				strcat(msg, data);
+				// TODO: checar se o usuario ainda esta conectado antes de enviar a mensagem
+				send(userList[i].sockID, msg, MAX_MSG, 0);
+			}
+		}
 	}
-	// envia AK
-	if(send(connfd, "AK", strlen("AK"), 0) <= 0){
-		printf("erro no envio da resposta\n");
-		return -1;
-	}
 
-    return 0;
-}
-
-#define MAX_MSG_LENGTH 4096
-
-// Message type
-typedef struct Message_t {
-    char user[51];
-    int req;
-    char content[MAX_MSG_LENGTH];
-} Message_t;
-
-typedef struct messageBuffer_t{
-	Message_t item[10];
-	int size;
-} messageBuffer_t;
-
-messageBuffer_t buf;
-
-// Semaphore variables
-sem_t x, y;
-pthread_t tid;
-pthread_t writerthreads[100];
-pthread_t readerthreads[100];
-int readercount = 0;
-
-// Reader Function
-void* reader(void* param)
-{
-	// Lock the semaphore
-	sem_wait(&x);
-	readercount++;
-
-	if (readercount == 1)
-		sem_wait(&y);
-
-	// Unlock the semaphore
-	sem_post(&x);
-
-	printf("%d Reader is inside\n",
-		readercount);
-
-	char input[4096];
-
-	int newSocket = *((int*)param);
-
-	send(newSocket,
-		&buf.item[buf.size].user, sizeof(buf.item[buf.size].user), 0);
-	
-	send(newSocket,
-		&buf.item[buf.size].content, sizeof(buf.item[buf.size].content), 0);
-	
-	// Lock the semaphore
-	sem_wait(&x);
-	readercount--;
-	buf.size--;
-
-	if (readercount == 0) {
-		sem_post(&y);
-	}
-
-	// Lock the semaphore
-	sem_post(&x);
-
-	printf("%d Reader is leaving\n",
-		readercount + 1);
-	pthread_exit(NULL);
-}
-
-// Writer Function
-void* writer(void* param)
-{
-	printf("Writer is trying to enter\n");
-
-	// Lock the semaphore
-	sem_wait(&y);
-
-	printf("Writer has entered\n");
-
-	int newSocket = *((int*)param);
-	int erro = 0;
-	char input[4096];
-
-	recv(newSocket,
-		&input, sizeof(input), 0);
-	printf("user: %s\n", input);
-
-	strcpy(buf.item[buf.size].user, input);
-
-	bzero(input, sizeof(input));
-
-	input[0] = '\0';
-	recv(newSocket,
-		&input, sizeof(input), 0);
-	printf("content: %s\n", input);
-	strcpy(buf.item[buf.size].content, input);
-	
-
-	erro = myRead(newSocket, input);
-	printf("%s\n", input);
-	bzero(input, sizeof(input));
-	erro = myRead(newSocket, input);
-	printf("%s\n", input);
-	bzero(input, sizeof(input));
-	erro = myRead(newSocket, input);
-	printf("%s\n", input);
-	bzero(input, sizeof(input));
-
-	// Unlock the semaphore
-	sem_post(&y);
-
-	printf("Writer is leaving\n");
-
-	buf.size++;
-	
 	pthread_exit(NULL);
 }
 
@@ -152,74 +96,17 @@ void interruptionHandler(int dummy) {
     printf("\n'Ctrl + C' não é um comando válido!!\n");
 }
 
-// Auxiliary function to split strings
-char** str_split(char* a_str, const char a_delim)
-{
-    char** result    = 0;
-    size_t count     = 0;
-    char* tmp        = a_str;
-    char* last_comma = 0;
-    char delim[2];
-    delim[0] = a_delim;
-    delim[1] = 0;
-
-    /* Count how many elements will be extracted. */
-    while (*tmp)
-    {
-        if (a_delim == *tmp)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
-    }
-
-    /* Add space for trailing token. */
-    count += last_comma < (a_str + strlen(a_str) - 1);
-
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
-    count++;
-
-    result = malloc(sizeof(char*) * count);
-
-    if (result)
-    {
-        size_t idx  = 0;
-        char* token = strtok(a_str, delim);
-
-        while (token)
-        {
-            assert(idx < count);
-            *(result + idx++) = strdup(token);
-            token = strtok(0, delim);
-        }
-        assert(idx == count - 1);
-        *(result + idx) = 0;
-    }
-
-    return result;
-}
-
-
-
-
 // Driver Code
 int main()
 {
-	// Initialize variables
-	int serverSocket, newSocket;
-	struct sockaddr_in serverAddr;
-	struct sockaddr_storage serverStorage;
-
 	// set handler function
 	//signal(SIGINT, interruptionHandler);
 
-	socklen_t addr_size;
-	sem_init(&x, 0, 1);
-	sem_init(&y, 0, 1);
-
+	// Initialize variables
+	int serverSocket;
 	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	struct sockaddr_in serverAddr;
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(8989);
@@ -238,98 +125,25 @@ int main()
 	else
 		printf("Error\n");
 
-    //printf("IP: %d\tPort:%d\n", serverAddr.sin_addr.s_addr, serverAddr.sin_port);
-
-	// Array for thread
-	pthread_t tid[60];
-
-	int i = 0;
-
 	while (1) {
-		addr_size = sizeof(serverStorage);
-
 		// Extract the first
 		// connection in the queue
-		newSocket = accept(serverSocket,
-						(struct sockaddr*)&serverStorage,
-						&addr_size);
+		userList[user_count].sockID = accept(serverSocket,
+			(struct sockaddr*)&userList[user_count].socketAddr,
+			&userList[user_count].len);
+		userList[user_count].index = user_count;
 
-		int choice = 0;
-		recv(newSocket,
-			&choice, sizeof(choice), 0);
+		if (pthread_create(&thread[user_count], NULL,
+			clientThread, (void *) &userList[user_count]) != 0)
+			// Error in creating thread
+			printf("Failed to create thread\n");
+		
 
-		if (choice == 1) {
-			// Creater readers thread
-			if (pthread_create(&readerthreads[i++], NULL,
-							reader, &newSocket)
-				!= 0)
-
-				// Error in creating thread
-				printf("Failed to create thread\n");
-		}
-		else if (choice == 2) {
-			// Create writers thread
-			if (pthread_create(&writerthreads[i++], NULL,
-							writer, &newSocket)
-				!= 0)
-
-				// Error in creating thread
-				printf("Failed to create thread\n");
-		} 
-		else if (choice == 3) {
-
-			char command[4096];
-			recv(newSocket,
-				&command, sizeof(command), 0);
-			printf("%s\n", command);
-			if (strncmp(command, "/ping", 5) == 0) {
-				bzero(command, sizeof(command));
-				strcpy(command, "pong\0");
-				send(newSocket, command, strlen(command), 0);
-				
-			}else if (strncmp(command, "/join", 5) == 0) {
-				printf("sera implementado no futuro.\n");
-				send(newSocket, "WIP", strlen("WIP")+1, 0);
-
-			}else if (strncmp(command, "/nickname", 9) == 0) {
-				printf("sera implementado no futuro.\n");
-				send(newSocket, "WIP", strlen("WIP")+1, 0);
-				// sera implementado deposi durante a parte dos canais
-			}else if (strncmp(command, "/kick", 5) == 0) {
-				printf("sera implementado no futuro.\n");
-				send(newSocket, "WIP", strlen("WIP")+1, 0);
-				// sera implementado duante a parte dos canais
-			}else if (strncmp(command, "/mute", 5) == 0) {
-				printf("sera implementado no futuro.\n");
-				send(newSocket, "WIP", strlen("WIP")+1, 0);
-				// sera implementado durante a parte dos canais
-			}else if (strncmp(command, "/unmute", 7) == 0) {
-				printf("sera implementado no futuro.\n");
-				send(newSocket, "WIP", strlen("WIP")+1, 0);
-				// sera implementado durante a parte dos canais
-			}
-
-		}
-
-		if (i >= 50) {
-			// Update i
-			i = 0;
-
-			while (i < 50) {
-				// Suspend execution of
-				// the calling thread
-				// until the target
-				// thread terminates
-				pthread_join(writerthreads[i++],
-							NULL);
-				pthread_join(readerthreads[i++],
-							NULL);
-			}
-
-			// Update i
-			i = 0;
-		}
+		user_count++;
 	}
+
+	for (int i=0; i<user_count; i++)
+		pthread_join(thread[i], NULL);
 
 	return 0;
 }
